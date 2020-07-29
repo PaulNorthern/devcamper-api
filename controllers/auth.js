1b6eb0b3ec75e6309aea2dcde92a053c4d117a08
PaulNorthern <sculltakers111@gmail.com>
2020-07-29 14:11:46 +0300

    Add a User model with register

---
+++ controllers/auth.js
@@ -0,0 +1,21 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
+
+// @desc      Refister user
+// @route     GET /api/v1/auth/register
+// @access    Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  // MONGOOSE returns a promise
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  res.status(200).json({ success: true });
+});
